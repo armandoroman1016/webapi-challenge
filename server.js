50821458285ebbadaa7ec0e183be156e015ee7ce
Armando Roman <armandoroman1016@gmail.com>
2019-09-06 09:57:10 -0700

    mvp

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const actionRoutes = require('./routes/actionsRoutes')
+const projectsRoutes = require('./routes/projectsRoutes')
 
 const server = express();
 
 server.use(express.json())
 server.use('/api/actions', actionRoutes)
+server.use('/api/projects', projectsRoutes)
 
 module.exports = server
